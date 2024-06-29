01948f3be6cc36e2d9d3877933063fde3954311a
gun2 <0dayz@naver.com>
2024-06-29 23:55:55 +0900

    eureka client에서 연결된 eureka instance 정보를 로그로 출력하는 스케줄러 추가

--- app/eureka-client/src/main/java/com/github/gun2/eurekaclient/scheduler/ChangeMetadataScheduler.java
+++ app/eureka-client/src/main/java/com/github/gun2/eurekaclient/scheduler/ChangeMetadataScheduler.java
@@ -4,9 +4,12 @@ import com.netflix.appinfo.ApplicationInfoManager;
 import com.netflix.appinfo.InstanceInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -16,6 +19,7 @@ import java.util.concurrent.atomic.AtomicLong;
 public class ChangeMetadataScheduler {
     private final AtomicLong atomicLong = new AtomicLong();
     private final ApplicationInfoManager applicationInfoManager;
+    private final DiscoveryClient discoveryClient;
     @Scheduled(fixedDelay = 1000)
     public void changeMetadata(){
         InstanceInfo info = applicationInfoManager.getInfo();
@@ -23,5 +27,16 @@ public class ChangeMetadataScheduler {
         metadataMap.put("count", String.valueOf(atomicLong.getAndIncrement()));
         applicationInfoManager.registerAppMetadata(metadataMap);
     }
+
+    @Scheduled(fixedDelay = 1000)
+    public void retrievalConnectedInstance(){
+        List<ServiceInstance> serviceInstances = discoveryClient.getServices().stream().flatMap(serviceId -> discoveryClient.getInstances(serviceId).stream())
+                .toList();
+        serviceInstances.forEach(serviceInstance -> {
+            log.info("serviceInstance : {}", serviceInstance);
+        });
+    }
+
+
 
 }
