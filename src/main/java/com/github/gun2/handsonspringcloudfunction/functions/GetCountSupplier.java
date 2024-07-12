6d5b500e0e1020a3e991342c6415b6d9d3f1827f
gun2 <0dayz@naver.com>
2024-07-12 23:35:24 +0900

    class를 통해 빈을 생성하여 function을 작성하는 예시 코드 추가

---
+++ src/main/java/com/github/gun2/handsonspringcloudfunction/functions/GetCountSupplier.java
@@ -0,0 +1,17 @@
+package com.github.gun2.handsonspringcloudfunction.functions;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Supplier;
+
+@Component(value = "get-count")
+@RequiredArgsConstructor
+public class GetCountSupplier implements Supplier<Long> {
+    private final CountComponent countComponent;
+
+    @Override
+    public Long get() {
+        return countComponent.get();
+    }
+}
---
+++ src/main/java/com/github/gun2/handsonspringcloudfunction/functions/ReverseStringFunction.java
@@ -0,0 +1,14 @@
+package com.github.gun2.handsonspringcloudfunction.functions;
+
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+//bean 이름으로 url path를 지정할 수 있음
+@Component(value = "reverse-string")
+public class ReverseStringFunction implements Function<String, String> {
+    @Override
+    public String apply(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+}
