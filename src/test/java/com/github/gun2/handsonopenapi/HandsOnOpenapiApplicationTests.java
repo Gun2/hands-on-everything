cddb3122baf12afae570b7d9aaebcec498e55637
gun2 <0dayz@naver.com>
2024-07-18 21:48:31 +0900

    first commit

---
+++ src/test/java/com/github/gun2/handsonopenapi/HandsOnOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gun2.handsonopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnOpenapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
