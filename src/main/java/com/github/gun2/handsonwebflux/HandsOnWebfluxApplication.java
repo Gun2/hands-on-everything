46c2331bfc4ce8993b1a4d0783c73a7cd36782ae
gun2 <0dayz@naver.com>
2024-07-19 23:29:15 +0900

    init

---
+++ src/main/java/com/github/gun2/handsonwebflux/HandsOnWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.github.gun2.handsonwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandsOnWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/gun2/handsonwebflux/HandsOnWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gun2.handsonwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
