b1c7454e257a2ae733eeb7bebc232ef3f978bb6c
gun2 <0dayz@naver.com>
2024-08-08 00:27:25 +0900

    일부 ShellMethod를 CommandRegistration의 빌더 패턴 구현 방식으로 변경

--- src/main/java/com/github/gun2/handsonspringshell/command/BoardCommand.java
+++ src/main/java/com/github/gun2/handsonspringshell/command/BoardCommand.java
@@ -3,6 +3,8 @@ package com.github.gun2.handsonspringshell.command;
 import com.github.gun2.handsonspringshell.dto.Board;
 import com.github.gun2.handsonspringshell.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.shell.command.CommandRegistration;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
@@ -14,8 +16,9 @@ import java.util.stream.Collectors;
 @ShellComponent
 @RequiredArgsConstructor
 public class BoardCommand {
+    private static final String GROUP = "Board Command";
     private final BoardRepository boardRepository;
-    @ShellMethod(key = "create", prefix = "-", value = "게시글 생성")
+    @ShellMethod(key = "create", prefix = "-", value = "게시글 생성", group = GROUP)
     public String create(
             @ShellOption(
                     value = {"t", "title"},
@@ -34,7 +37,7 @@ public class BoardCommand {
                 %s""".formatted((boardToText(board)));
     }
 
-    @ShellMethod(key = "read", prefix = "-", value = "id에 해당하는 게시글을 조회")
+    @ShellMethod(key = "read", prefix = "-", value = "id에 해당하는 게시글을 조회", group = GROUP)
     public String read(
             @ShellOption(help = "조회할 게시글의 id를 입력합니다") String id
     ) {
@@ -50,7 +53,7 @@ public class BoardCommand {
                 %s""".formatted((boardToText(boardOptional.get())));
     }
 
-    @ShellMethod(key = "list", prefix = "-", value = "모든 게시글 조회")
+    @ShellMethod(key = "list", prefix = "-", value = "모든 게시글 조회", group = GROUP)
     public String list() {
         List<Board> boards = boardRepository.readAll();
         return """
@@ -58,41 +61,47 @@ public class BoardCommand {
                 %s""".formatted(boards.stream().map(this::boardToText).collect(Collectors.joining("\r\n--------------------------------------------\r\n")));
     }
 
-    @ShellMethod(key = "update", prefix = "-", value = "id에 해당하는 게시글을 수정")
-    public String update(
-            @ShellOption(
-                    value = {"id"},
-                    help = "수정할 게시글의 id를 입력합니다."
-            ) Long id,
-            @ShellOption(
-                    value = {"t", "title"},
-                    help = "수정할 게시글의 제목을 입력합니다."
-            ) String title,
-            @ShellOption(
-                    value = {"c", "content"},
-                    help = "수정할 게시글의 내용을 입력합니다."
-            ) String content
+    @Bean
+    CommandRegistration update() {
+        return CommandRegistration.builder()
+                .group(GROUP)
+                .command("update")
+                .description("id에 해당하는 게시글을 수정")
+                .withHelpOptions().shortNames('h').longNames("help").command("help").and()
+                .withOption().required().type(Long.TYPE).longNames("id").description("수정할 게시글의 id를 입력합니다.").and()
+                .withOption().shortNames('t').longNames("title").description("수정할 게시글의 제목을 입력합니다.").and()
+                .withOption().shortNames('c').longNames("content").description("수정할 게시글의 내용을 입력합니다.").and()
+                .withTarget().function(commandContext -> {
+                    Long id = commandContext.getOptionValue("id");
+                    String title = commandContext.getOptionValue("title");
+                    String content = commandContext.getOptionValue("content");
+                    Board board = boardRepository.update(id, title, content);
 
-    ) {
-        Board board = boardRepository.update(id, title, content);
-
-        return """
-                게시글을 수정하였습니다.
-                %s""".formatted((boardToText(board)));
+                    return """
+                            게시글을 수정하였습니다.
+                            %s""".formatted((boardToText(board)));
+                }).and()
+                .build();
     }
 
+    @Bean
+    CommandRegistration delete() {
+        return CommandRegistration.builder()
+                .group(GROUP)
+                .command("delete")
+                .description("id에 해당하는 게시글을 삭제")
+                .withHelpOptions().shortNames('h').longNames("help").command("help").and()
+                .withOption().required().type(Long.TYPE).longNames("id").description("삭제할 게시글의 id를 입력합니다.").and()
+                .withTarget().function(commandContext -> {
+                    Long id = commandContext.getOptionValue("id");
+                    boardRepository.delete(id);
 
-    @ShellMethod(key = "delete", prefix = "-", value = "id에 해당하는 게시글을 삭제")
-    public String delete(
-            @ShellOption(help = "삭제할 게시글의 id를 입력합니다.") Long id
-    ) {
-        boardRepository.delete(id);
-
-        return """
-                게시글을 삭제하였습니다.
-                """;
+                    return """
+                            게시글을 삭제하였습니다.
+                            """;
+                }).and()
+                .build();
     }
-
 
     private String boardToText(Board board) {
         return """
