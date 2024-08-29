cdcaba3c80e3112718f0a35eb4f01bf320c82550
gun2 <0dayz@naver.com>
2024-08-29 22:47:11 +0900

    get, post 메서드의 동기/비동기 샘플 및 설명 추가

--- hands-on-httpclient5/README.md
+++ hands-on-httpclient5/README.md
@@ -1,10 +1,91 @@
-# HttpClient5
+# HttpClient5.3
+HttpCore 기반으로 구현된 HTTP 에이전트로서 `client side 인증`, `상태관리`, `연결관리` 등을 재공하는 재사용가능한 컴포넌트
 
 ## Quick start
 ### 의존성 추가
 ```groovy
 implementation 'org.apache.httpcomponents.client5:httpclient5:5.3.1'
 ```
+
+### Get 요청 (동기)
+```java
+public void get(){
+    try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+        ClassicHttpRequest httpGet = ClassicRequestBuilder.get("http://httpbin.org/get")
+                .build();
+        httpclient.execute(httpGet, response -> {
+            System.out.println(response.getCode() + " " + response.getReasonPhrase());
+            final HttpEntity entity1 = response.getEntity();
+            EntityUtils.consume(entity1);
+            return null;
+        });
+    }
+}
+```
+
+### Post 요청 (동기)
+```java
+public void post(){
+    try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+        ClassicHttpRequest httpPost = ClassicRequestBuilder.post("http://httpbin.org/post")
+                .setEntity(new UrlEncodedFormEntity(Arrays.asList(
+                        new BasicNameValuePair("username", "vip"),
+                        new BasicNameValuePair("password", "secret"))))
+                .build();
+        httpclient.execute(httpPost, response -> {
+            System.out.println(response.getCode() + " " + response.getReasonPhrase());
+            final HttpEntity entity2 = response.getEntity();
+            EntityUtils.consume(entity2);
+            return null;
+        });
+    }
+}
+```
+
+### Get 요청 (비동기)
+```java
+public void asyncGet(){
+    try (CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault()) {
+        httpclient.start();
+        SimpleHttpRequest request1 = SimpleRequestBuilder.get("http://httpbin.org/get").build();
+        Future<SimpleHttpResponse> future = httpclient.execute(request1, null);
+        //결과 반환
+        SimpleHttpResponse response1 = future.get();
+    }
+}
+```
+
+### Post 요청 (비동기)
+```java
+public void asyncPost(){
+    try (CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault()) {
+        // Start the client
+        httpclient.start();
+
+        SimpleHttpRequest request1 = SimpleRequestBuilder.post("http://httpbin.org/post")
+                .addParameters(
+                        new BasicNameValuePair("username", "vip"),
+                        new BasicNameValuePair("password", "secret")
+                ).build();
+        Future<SimpleHttpResponse> future = httpclient.execute(request1, null);
+        SimpleHttpResponse simpleHttpResponse = future.get();
+    }
+}
+```
+
+
+## 기능
+- 표준 기반의 순수 Java로 구현된 HTTP 1.0, 1.1, 2.0(비동기 API만 지원) 지원
+- HTTPS(SSL을 통한 HTTP) 프로토콜을 통한 암호화 지원
+- 플러그형 소켓 팩토리 및 TLS 전략 지원
+- HTTP/1.1 및 HTTP/1.0 프록시를 통한 투명한 메시지 교환
+- CONNECT 메서드를 통해 HTTP/1.1 및 HTTP/1.0 프록시를 통한 터널링된 HTTPS 연결
+- Basic, Digest, Bearer 인증 스킴 지원
+- HTTP 상태 관리 및 쿠키 지원
+- 유연한 연결 관리 및 풀링 지원
+- HTTP 응답 캐싱 지원
+- 소스 코드는 Apache 라이선스 하에 무료로 제공
+
 ## 기록 사항
 ### 연결 중 소비되지 않은 응답값의 재사용은 안전하지 않다.
 > `NotFullyConsumeAndReuse.class` 코드 참고
--- hands-on-httpclient5/src/main/java/com/github/gun2/QuickStart.java
+++ hands-on-httpclient5/src/main/java/com/github/gun2/QuickStart.java
@@ -1,19 +1,42 @@
 package com.github.gun2;
 
+import org.apache.hc.client5.http.async.methods.AbstractCharResponseConsumer;
+import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
+import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
+import org.apache.hc.client5.http.async.methods.SimpleRequestBuilder;
 import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.ClassicHttpRequest;
-import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.concurrent.FutureCallback;
+import org.apache.hc.core5.http.*;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.support.ClassicRequestBuilder;
 import org.apache.hc.core5.http.message.BasicNameValuePair;
+import org.apache.hc.core5.http.nio.AsyncRequestProducer;
+import org.apache.hc.core5.http.nio.support.AsyncRequestBuilder;
 
 import java.io.IOException;
+import java.nio.CharBuffer;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class QuickStart {
     public static void main(String[] args) throws IOException {
+        getAndPost();
+        asyncGet();
+        asyncPost();
+    }
+
+    /**
+     * get 요청과 post 요청
+     * @throws IOException
+     */
+    private static void getAndPost() throws IOException {
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
             ClassicHttpRequest httpGet = ClassicRequestBuilder.get("http://httpbin.org/get")
                     .build();
@@ -46,6 +69,133 @@ public class QuickStart {
                 EntityUtils.consume(entity2);
                 return null;
             });
+        }
+    }
+
+    /**
+     * 비동기 요청
+     */
+    public static void asyncGet(){
+        try (CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault()) {
+            // Start the client
+            httpclient.start();
+
+            // Execute request
+            SimpleHttpRequest request1 = SimpleRequestBuilder.get("http://httpbin.org/get").build();
+            Future<SimpleHttpResponse> future = httpclient.execute(request1, null);
+            // and wait until response is received
+            SimpleHttpResponse response1 = future.get();
+            System.out.println(request1.getRequestUri() + "->" + response1.getCode());
+
+            // One most likely would want to use a callback for operation result
+            CountDownLatch latch1 = new CountDownLatch(1);
+            SimpleHttpRequest request2 = SimpleRequestBuilder.get("http://httpbin.org/get").build();
+            httpclient.execute(request2, new FutureCallback<SimpleHttpResponse>() {
+
+                @Override
+                public void completed(SimpleHttpResponse response2) {
+                    latch1.countDown();
+                    System.out.println(request2.getRequestUri() + "->" + response2.getCode());
+                }
+
+                @Override
+                public void failed(Exception ex) {
+                    latch1.countDown();
+                    System.out.println(request2.getRequestUri() + "->" + ex);
+                }
+
+                @Override
+                public void cancelled() {
+                    latch1.countDown();
+                    System.out.println(request2.getRequestUri() + " cancelled");
+                }
+
+            });
+            latch1.await();
+
+            // In real world one most likely would want also want to stream
+            // request and response body content
+            CountDownLatch latch2 = new CountDownLatch(1);
+            AsyncRequestProducer producer3 = AsyncRequestBuilder.get("http://httpbin.org/get").build();
+            AbstractCharResponseConsumer<HttpResponse> consumer3 = new AbstractCharResponseConsumer<HttpResponse>() {
+
+                HttpResponse response;
+
+                @Override
+                protected void start(HttpResponse response, ContentType contentType) throws HttpException, IOException {
+                    this.response = response;
+                }
+
+                @Override
+                protected int capacityIncrement() {
+                    return Integer.MAX_VALUE;
+                }
+
+                @Override
+                protected void data(CharBuffer data, boolean endOfStream) throws IOException {
+                    // Do something useful
+                    System.out.println("data : " + data);
+                    System.out.println("endOfStream : " + endOfStream);
+                }
+
+                @Override
+                protected HttpResponse buildResult() throws IOException {
+                    return response;
+                }
+
+                @Override
+                public void releaseResources() {
+                    System.out.println("releaseResources");
+                }
+
+            };
+            httpclient.execute(producer3, consumer3, new FutureCallback<HttpResponse>() {
+
+                @Override
+                public void completed(HttpResponse response3) {
+                    latch2.countDown();
+                    System.out.println(request2.getRequestUri() + "->" + response3.getCode());
+                }
+
+                @Override
+                public void failed(Exception ex) {
+                    latch2.countDown();
+                    System.out.println(request2.getRequestUri() + "->" + ex);
+                }
+
+                @Override
+                public void cancelled() {
+                    latch2.countDown();
+                    System.out.println(request2.getRequestUri() + " cancelled");
+                }
+
+            });
+            latch2.await();
+
+        } catch (IOException | InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void asyncPost(){
+        try (CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault()) {
+            // Start the client
+            httpclient.start();
+
+            SimpleHttpRequest request1 = SimpleRequestBuilder.post("http://httpbin.org/post")
+                    .addParameters(
+                            new BasicNameValuePair("username", "vip"),
+                            new BasicNameValuePair("password", "secret")
+                    ).build();
+            Future<SimpleHttpResponse> future = httpclient.execute(request1, null);
+            SimpleHttpResponse simpleHttpResponse = future.get();
+            System.out.println("simpleHttpResponse : " + simpleHttpResponse.getBodyText());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
