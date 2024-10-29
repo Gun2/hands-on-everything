7d618f36da71a31ad234804788f2a7364adfb797
gun2 <0dayz@naver.com>
2024-10-29 23:43:11 +0900

    flyway 핸즈온 프로젝트 초기 커밋

---
+++ hands-on-flyway/README.md
@@ -0,0 +1,2 @@
+# Hands On Flyway
+flyway 핸즈온 프로젝트
---
+++ hands-on-flyway/src/main/java/com/github/gun2/handsonflyway/HandsOnFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.github.gun2.handsonflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnFlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandsOnFlywayApplication.class, args);
+    }
+
+}
