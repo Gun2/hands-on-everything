b07a2d760c748653ef65135d77855705deea4f20
gun2 <0dayz@naver.com>
2024-11-01 23:43:23 +0900

    flyway 샘플 마이그레이션 추가

---
+++ hands-on-flyway/src/main/resources/db/migration/V1__Create_person_table.sql
@@ -0,0 +1,8 @@
+create table PERSON (
+        ID int not null,
+        NAME varchar(100) not null
+);
+
+INSERT INTO PERSON(ID, NAME) VALUES (1, 'JONE');
+INSERT INTO PERSON(ID, NAME) VALUES (2, 'JACKSON');
+INSERT INTO PERSON(ID, NAME) VALUES (3, 'ERIC');
