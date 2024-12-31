286290d9a4818c4687c1ea64b4738398656bb5d8
gun2 <0dayz@naver.com>
2025-01-01 00:04:37 +0900

    fixup! 게시판 정보 변경 시 변경 정보를 전달하는 websocket 추가

--- hands-on-rtk-query/server/src/main/java/com/github/gun2/server/websocket/WebSocketConfig.java
+++ hands-on-rtk-query/server/src/main/java/com/github/gun2/server/websocket/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws") // 클라이언트가 연결할 엔드포인트
-                .setAllowedOrigins("*") // 허용된 CORS 설정
+                .setAllowedOriginPatterns("*") // 허용된 CORS 설정
                 .withSockJS(); // SockJS fallback 사용
     }
 }
