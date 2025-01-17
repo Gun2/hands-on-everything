a90ae7517ec8c3d1482d2d7dcb638b482b38b9ee
gun2 <0dayz@naver.com>
2025-01-17 23:03:16 +0900

    expo tutorial - navigation

---
+++ hands-on-react-native-with-expo/app/(tabs)/index.tsx
@@ -0,0 +1,34 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router';
+
+export default function Index() {
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
