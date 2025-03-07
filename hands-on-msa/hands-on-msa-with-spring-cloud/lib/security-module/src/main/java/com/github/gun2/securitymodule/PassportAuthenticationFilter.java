b7c9155e91283d43cfa292ffd72dde0ded118518
gun2 <0dayz@naver.com>
2025-03-08 03:45:00 +0900

    fixup! fixup! fixup! spring cloud - passport module 추가

--- hands-on-msa/hands-on-msa-with-spring-cloud/lib/security-module/src/main/java/com/github/gun2/securitymodule/PassportAuthenticationFilter.java
+++ hands-on-msa/hands-on-msa-with-spring-cloud/lib/security-module/src/main/java/com/github/gun2/securitymodule/PassportAuthenticationFilter.java
@@ -6,12 +6,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -33,7 +34,7 @@ public class PassportAuthenticationFilter extends OncePerRequestFilter {
                     String role = passportUtil.extract(token, ClaimNames.ROLE);
 
                     UsernamePasswordAuthenticationToken authentication =
-                            new UsernamePasswordAuthenticationToken(username, token, AuthorityUtils.createAuthorityList(role));
+                            new UsernamePasswordAuthenticationToken(username, token, List.of(new SimpleGrantedAuthority("ROLE_"+role)));
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
             }
