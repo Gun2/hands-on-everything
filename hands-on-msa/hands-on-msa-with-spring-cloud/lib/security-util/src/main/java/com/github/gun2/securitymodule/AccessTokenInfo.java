60942f0fca8ad951ac9014334af4c7dce12690f2
gun2 <0dayz@naver.com>
2025-04-07 23:58:54 +0900

    spring cloud - 불필요한 파일 삭제

--- hands-on-msa/hands-on-msa-with-spring-cloud/lib/security-util/src/main/java/com/github/gun2/securitymodule/AccessTokenInfo.java
+++
@@ -1,6 +0,0 @@
-package com.github.gun2.securitymodule;
-
-public class AccessTokenInfo {
-    public static final String HEADER_NAME = "Authorization";
-    public static final String TOKEN_PREFIX = "Bearer";
-}
