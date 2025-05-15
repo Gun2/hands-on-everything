67b7875a9f2153d895a88828f7e4640867bc0c56
gun2 <0dayz@naver.com>
2025-05-16 03:07:53 +0900

    fixup! spring cloud - 레이아웃 헤더에 현재 시간 추가

--- hands-on-msa/hands-on-msa-with-spring-cloud/app/ui-app/src/app/(auth)/(components)/Header.tsx
+++ hands-on-msa/hands-on-msa-with-spring-cloud/app/ui-app/src/app/(auth)/(components)/Header.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import LogoutButton from '@/app/(auth)/(components)/LogoutButton';
 import CurrentTime from '@/app/(auth)/(components)/CurrentTime';
+import { auth } from '../../../../auth';
 
 type HeaderProps = {
 }
-const Header = (
+const Header = async (
   {
 
   }: HeaderProps
 ) => {
+  let session = await auth();
+  if (session == null) {
+    return;
+  }
   return (
     <div style={{ flex: 1, textAlign: "end", fontSize: "1.5rem" }}>
       <CurrentTime/>
