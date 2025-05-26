b6f821f29f4a1bbe211ec3f34d110cfa112c16bb
gun2 <0dayz@naver.com>
2025-05-26 23:31:08 +0900

    fixup! spring kafka - 기본 구성 추가

---
+++ hands-on-kafka-with-spring/src/main/java/com/github/gun2/handsonkafkawithspring/HandsOnKafkaWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.gun2.handsonkafkawithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnKafkaWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandsOnKafkaWithSpringApplication.class, args);
+    }
+
+}
---
+++ hands-on-kafka-with-spring/src/test/java/com/github/gun2/handsonkafkawithspring/HandsOnKafkaWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gun2.handsonkafkawithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnKafkaWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
