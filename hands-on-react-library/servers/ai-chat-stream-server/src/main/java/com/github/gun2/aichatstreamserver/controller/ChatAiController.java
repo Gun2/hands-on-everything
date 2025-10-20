73449dba3e5f81d5520b2ead06d5c70f5dbc60ea
gun2 <0dayz@naver.com>
2025-10-21 00:19:14 +0900

    chatscope chat ui kit - 샘플 비동기 ai chat api 서버 추가

---
+++ hands-on-react-library/servers/ai-chat-stream-server/src/main/java/com/github/gun2/aichatstreamserver/controller/ChatAiController.java
@@ -0,0 +1,34 @@
+package com.github.gun2.aichatstreamserver.controller;
+
+import com.github.gun2.aichatstreamserver.dto.ClientChatRequest;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/stream-chat-ai")
+public class ChatAiController {
+
+    private final ChatClient chatClient;
+
+    public ChatAiController(ChatMemory chatMemory, ChatModel chatModel) {
+        this.chatClient = ChatClient.builder(chatModel)
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
+    }
+
+
+    @PostMapping
+    public Flux<String> chat(@RequestBody ClientChatRequest chatRequest) {
+        return this.chatClient.prompt(chatRequest.getContent()).stream().content();
+    }
+}
